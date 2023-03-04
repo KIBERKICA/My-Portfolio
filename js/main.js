f8fa3e528f7bbe88c80a5d2a08d482c6b2d3ee99
Kirill Chernov <87560228+KIBERKICA@users.noreply.github.com>
2023-03-05 00:28:53 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@ $('button[data-filter]').on('click', (e)=>
   $('li', content).show();
   if (filter) $('li:not(.' + filter.split(' ').join(', .') + ')', content).hide()
 });
+
+function lightmode() {
+  const body = document.body
+  const wasLightmode = localStorage.getItem('lightmode') === 'true'
+
+  localStorage.setItem('lightmode', !wasLightmode)
+  body.classList.toggle('wrapper-light', !wasLightmode)
+}
+
+document.querySelector('.theme').addEventListener('click', lightmode)
+
+function onload() {
+  document.body.classList.toggle('wrapper-light', localStorage.getItem('lightmode') === 'true')
+}
+document.addEventListener('DOMContentLoaded', onload)
